75dd41250c551cd67fab1d19997b1d956d4b6e43
Henrique <henriquechigumane@outlook.com>
2020-09-08 17:05:00 +0200

    add picsum api connection

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const galleryBtn= document.getElementById("gallery__btn");
+const template = document.querySelector(".gallery__figure");
+const figure=document.getElementById("123");
+const list= figure.closest("ul");
+
+function createRequest(url,method) {
+    return fetch(url).then(response=>{return response.json();});
+}
+ 
+
+async function loadImages() {
+    let i=0
+    const images= await createRequest("https://picsum.photos/v2/list");
+    console.log(images);
+    for(const image of images){
+        const newImage = document.importNode(figure, true);
+        console.log(newImage);
+        newImage.querySelector("img").src=image.download_url;
+        newImage.querySelector(".gallery__img-caption > span").innerHTML=image.author;
+
+        list.append(newImage);
+        if(i==6) return;
+        i++;
+    
+    }
+}
+function createNode(id,author,src) {
+  
+}
+
+galleryBtn.addEventListener("click", event=>{
+    event.preventDefault();
+    loadImages();
+
+})
+
